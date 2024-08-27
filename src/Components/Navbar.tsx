53fbb13f68b80274758364e6e66c32cb1cdaf3ba
sushmasri1906 <sushmasri1906@gmail.com>
2024-08-27 11:53:25 +0530

    nav bar responsive

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -21,25 +21,25 @@ const Navbar = () => {
 	};
 
 	return (
-		<nav className="flex items-center justify-between p-4 bg-orange-200 text-gray-800 shadow-md">
-			<div className="flex items-center space-x-4">
+		<nav className="flex items-center justify-between p-4 bg-orange-200 text-gray-800 shadow-md  gap-x-2 sm:gap-x-4">
+			<div className="flex items-center gap-x-2 sm:gap-x-4">
 				<Link to="/">
-					<img src={logo} alt="Logo" className="h-12 w-12" />
+					<img src={logo} alt="Logo" className="h-auto max-h-12 w-12" />
 				</Link>
 				<Link to="/" className="font-bold text-xl">
 					Hexashop
 				</Link>
 			</div>
 
-			<div className="flex items-center space-x-4">
-				<div className="relative flex-1">
+			<div className="flex items-center justify-center gap-x-2 sm:gap-x-4">
+				{/* <div className="relative flex-1">
 					<input
 						type="text"
 						className="pl-10 pr-4 py-2 rounded-full bg-white text-gray-800 focus:outline-none shadow-md w-full max-w-xs"
 						placeholder="Search..."
 					/>
 					<FaSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 text-orange-400 text-xl bg-orange-200 rounded-full p-1 hover:bg-orange-300 transition-colors duration-300" />
-				</div>
+				</div> */}
 
 				<Link to="/wishlist">
 					<FaHeart className="text-xl cursor-pointer text-white bg-orange-600 rounded-full p-1 hover:bg-orange-300 transition-colors duration-300" />
@@ -51,13 +51,16 @@ const Navbar = () => {
 
 				{isLoggedIn ? (
 					<>
-						<button onClick={handleLogout} className="px-4 py-2 bg-orange-600 text-white rounded-full hover:bg-orange-500 transition-colors duration-300 shadow-md">
+						<button
+							onClick={handleLogout}
+							className="px-4 py-2 bg-orange-600 text-white rounded-full hover:bg-orange-500 transition-colors duration-300 shadow-md">
 							Logout
 						</button>
-						<Link to="/profile" className="text-xl text-gray-800 hover:text-orange-600 transition-colors duration-300">
+						<Link
+							to="/profile"
+							className="text-xl text-gray-800 hover:text-orange-600 transition-colors duration-300">
 							<FaUser className="text-xl text-orange-600 border-2 border-orange-600 rounded-full p-1 hover:bg-orange-300 transition-colors duration-300" />
 						</Link>
-						
 					</>
 				) : (
 					<>
@@ -74,4 +77,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-
